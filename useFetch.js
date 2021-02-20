d3179774205b8d2796cbe7b07161ed70188572c9
joelbarreraarotoma@gmail.com <joelbarreraarotoma@gmail.com>
2021-02-19 19:04:11 -0500

    primer comit

---
+++ useFetch.js
@@ -0,0 +1,42 @@
+//este hook sirve para ser usado en multiplecustomhook
+
+import {useState,useEffect, useRef} from "react";
+
+export const useFetch = (url) => {
+    
+    const isMounted = useRef(true);
+    const [state, setstate] = useState({data:null,loading: true,error:null});
+
+    useEffect(()=>{
+        return()=>{
+            isMounted.current=false;
+        }
+
+    },[])
+
+    useEffect(() => {
+        setstate({data:null,loading:true,error:null});
+
+        fetch(url)
+        .then(resp=>resp.json())
+        .then(data=>{
+            setTimeout(() => {
+
+                if(isMounted.current){
+                    setstate({
+                       loading:false,
+                       error:null,
+                       data 
+                    })
+
+                }else{
+                    console.log('set state no se llamo');
+                }
+
+                
+            }, 4000);
+        })
+    }, [url])
+    return state;
+   
+}
